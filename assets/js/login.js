1838ff029f99301350a42d34a0bee467fc9705c0
Rose <663105594@qq.com>
2022-01-14 15:57:01 +0800

    提交

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,9 +1,9 @@
-$(function() {
-  $("#link_reg").on("click", function() {
+$(function () {
+  $("#link_reg").on("click", function () {
     $(".login-box").hide();
     $(".reg-box").show();
   });
-  $("#link_login").on("click", function() {
+  $("#link_login").on("click", function () {
     $(".login-box").show();
     $(".reg-box").hide();
   });
@@ -11,20 +11,20 @@ $(function() {
   var layer = layui.layer
   form.verify({
     pwd: [/^[\S]{6,12}$/, "密码必须6-12位,且不能带有空格"],
-    repwd: function(value) {
+    repwd: function (value) {
       var pwd = $(".reg-box [name = password]").val();
       if (pwd !== value) {
         return "两次密码不一致！！！！！";
       }
     }
   });
-  $("#form_reg").on("submit", function(e) {
+  $("#form_reg").on("submit", function (e) {
     e.preventDefault();
     var data = {
       username: $("#form_reg [name = username]").val(),
       password: $("#form_reg [name=password]").val()
     };
-    $.post("/api/reguser", data, function(res) {
+    $.post("/api/reguser", data, function (res) {
       if (res.status !== 0) {
         return layer.msg(res.message)
       }
@@ -33,17 +33,17 @@ $(function() {
       $('#link_login').click()
     });
   });
-  $('#form_login').submit(function(e){
+  $('#form_login').submit(function (e) {
     e.preventDefault()
     $.ajax({
-      url:'/api/login',
-      method:'POST',
+      url: '/api/login',
+      method: 'POST',
       // serialize()获取表单数据
       data: $(this).serialize(),
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
           // console.log(res);
-          
+
           return layer.msg('登录失败！')
         }
         layer.msg('登陆成功！')
